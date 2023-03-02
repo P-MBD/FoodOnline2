d624a0bb57fdbddc2d5d8454a0588034320c13b0
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-02 19:41:41 +0330

    Templates Inheritance Base HTML

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
