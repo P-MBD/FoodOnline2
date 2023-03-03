75b829c6719b3704fee683428dd813198f425c46
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-03 21:11:14 +0330

    Login Page Setup

--- accounts/views.py
+++ accounts/views.py
@@ -81,3 +81,12 @@ def registerVendor(request):
         'v_form': v_form,
     }
     return render(request,'accounts/registerVendor.html',context)
+
+def login(request):
+    return render(request,'accounts/login.html') 
+
+def logout(request):
+    return 
+
+def dashboard(request):
+    return 
