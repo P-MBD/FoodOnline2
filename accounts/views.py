e579e38f825437eb714a64933ff216c012594523
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-11 13:00:35 +0330

    Load Vendor Profile Image Dynamically

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 
 from django.core.exceptions import PermissionDenied
+from vendor.models import Vendor
 
 # Restrict the vendor from accessing Customer Page
 def check_role_vendor(user):
@@ -163,7 +164,11 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor= Vendor.objects.get(user=request.user)
+    context={
+        'vendor':vendor,
+    }
+    return render(request, 'accounts/vendorDashboard.html',context)
 
 def forgot_password(request):
     if request.method=='POST':
