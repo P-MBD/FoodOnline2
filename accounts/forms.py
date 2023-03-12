75a896938813bb96b5ed49a518b24f85a4ff33ff
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-12 19:15:50 +0330

    066)Custom Validator Function Allow Only Images

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password= forms.CharField(widget=forms.PasswordInput)
@@ -9,8 +10,9 @@ class UserForm(forms.ModelForm):
         fields=['first_name','last_name','username','email','phone_number','password']
 
 class UserProfileForm(forms.ModelForm):
-    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
-    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address_line_1','address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
--- vendor/views.py
+++ vendor/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from accounts.forms import  UserProfileForm
 from .forms import VendorForm
-from accounts.forms import UserProfileForm
-
 from accounts.models import UserProfile
 from .models import Vendor
+from django.contrib import messages
 # Create your views here.
 def vprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user) 
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Settings updated.')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance = profile)
+        vendor_form = VendorForm(instance=vendor)
 
-    profile_form = UserProfileForm()
-    vendor_form = VendorForm()
     context = {
         'profile_form': profile_form,
         'vendor_form': vendor_form,
@@ -19,4 +30,3 @@ def vprofile(request):
         'vendor': vendor,
     }
     return render(request, 'vendor/vprofile.html', context)
-    
