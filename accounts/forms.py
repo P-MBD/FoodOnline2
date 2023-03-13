86f419225f00c49dbc6c649cae88b89d1a8804cc
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-13 09:30:59 +0330

    067) Readonly Latlong and Apply Decorator

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,3 +16,8 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address_line_1','address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
