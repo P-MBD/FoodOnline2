13704b0f9c80d2ee121d983846e39c68aa51899e
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-19 11:07:21 +0330

    096)Marketplace Html

--- marketplace/views.py
+++ marketplace/views.py
@@ -6,9 +6,9 @@ class MarketPlace(TemplateView):
      template_name = "marketplace/listings.html"
     
      def get_context_data(self, **kwargs):
-	        context = super().get_context_data(**kwargs)
-	        context['vendors'] =  Vendor.objects.filter(is_approved=True, user__is_active=True )
-            
-	        return context
+        context = super().get_context_data(**kwargs)
+        context['vendor_count'] = Vendor.objects.count()
+        context['vendors'] =  Vendor.objects.filter(is_approved=True, user__is_active=True)
+        return context
     
 
