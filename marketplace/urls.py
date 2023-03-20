470be9cc49b9cbadbac71a58a365629625ed3915
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-20 08:26:58 +0330

    098)Vendor Detail Page Setup

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.MarketPlace.as_view(), name='marketplace'),
+    path('<slug:vendor_slug>/',views.VendorDetail.as_view(), name="vendor_detail")
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.views.generic.base import TemplateView
 from vendor.models import Vendor
+from django.views.generic import DetailView
 # Create your views here.
 class MarketPlace(TemplateView):
      template_name = "marketplace/listings.html"
@@ -11,4 +12,14 @@ class MarketPlace(TemplateView):
         context['vendors'] =  Vendor.objects.filter(is_approved=True, user__is_active=True)
         return context
     
+class VendorDetail(DetailView):
+   model= Vendor
+   slug_url_kwarg = 'vendor_slug'
+   slug_field = 'vendor_slug'
+  
+   template_name='marketplace/vendor_detail.html'
+   def get_context_data(self, **kwargs):
+        context = super(VendorDetail, self).get_context_data(**kwargs)  
+        context['vendor']  = get_object_or_404(Vendor, vendor_slug=self.kwargs['vendor_slug'])
+        return context
 
