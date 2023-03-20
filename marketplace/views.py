a97d1870265f29e83bd5c6e455bfc50b155c9a1b
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-03-20 17:54:22 +0330

    099)Display Fooditems By Category Using Prefetch Related

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404,render
 from django.views.generic.base import TemplateView
 from vendor.models import Vendor
+from menu.models import Category,FoodItem
 from django.views.generic import DetailView
+from django.db.models import Prefetch
 # Create your views here.
 class MarketPlace(TemplateView):
      template_name = "marketplace/listings.html"
@@ -18,8 +20,18 @@ class VendorDetail(DetailView):
    slug_field = 'vendor_slug'
   
    template_name='marketplace/vendor_detail.html'
+   
    def get_context_data(self, **kwargs):
+        vendor=get_object_or_404(Vendor, vendor_slug=self.kwargs['vendor_slug'])
         context = super(VendorDetail, self).get_context_data(**kwargs)  
-        context['vendor']  = get_object_or_404(Vendor, vendor_slug=self.kwargs['vendor_slug'])
+        context['vendor']  = vendor
+        context['categories']= Category.objects.filter(vendor=vendor).prefetch_related(Prefetch(
+
+         'fooditems',
+         queryset=FoodItem.objects.filter(is_available=True)
+
+        ))
+
+       
         return context
 
